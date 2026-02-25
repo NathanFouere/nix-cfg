32f69e72f656f18a8730e697c544d59834974c6a
Nathan Fouéré <nathanfouere@tutanota.com>
2026-02-25 12:43:05 +0100

    update zed conf

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -19,9 +19,28 @@
     ];
     userSettings = {
       hour_format = "hour24";
-      vim_mode = false;
+      vim_mode = true;
       show_edit_predictions = true;
       show_completions_on_input = true;
+      scrollbar = {
+        show = "never";
+      };
+      indent_guides = {
+        enabled = true;
+        coloring = "indent_aware";
+      };
+      inlay_hints = {
+        enabled = true;
+      };
+      project_panel = {
+        button = true;
+        dock = "right";
+        git_status = true;
+      };
+      centered_layout = {
+        left_padding = 0.15;
+        right_padding = 0.15;
+      };
       assistant = {
         enabled = true;
         provider = "copilot";
