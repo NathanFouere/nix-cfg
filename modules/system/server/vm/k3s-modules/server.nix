7e7019f0936a7989ad79e2b37d28d11bab7245d9
Nathan Fouéré <nathanfouere@tutanota.com>
2026-03-15 14:42:33 +0100

    fit lint

--- modules/system/server/vm/k3s-modules/server.nix
+++ modules/system/server/vm/k3s-modules/server.nix
@@ -9,5 +9,13 @@
       tokenFile = "/run/agenix/k3s-token";
       clusterInit = true;
     };
+
+    environment.systemPackages = with pkgs; [
+      kubernetes-helm
+      fluxcd
+      git
+      kubectl
+      k9s
+    ];
   };
 }
