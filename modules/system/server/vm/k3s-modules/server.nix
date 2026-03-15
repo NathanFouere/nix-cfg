65e49b6f433e7a12e12563cec0efd231ed0f6bfe
Fouere Nathan <nathanfouere@tutanota.com>
2026-03-15 16:13:12 +0100

    add flux

--- modules/system/server/vm/k3s-modules/server.nix
+++ modules/system/server/vm/k3s-modules/server.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   ...
 }:
 {
@@ -17,5 +18,34 @@
       kubectl
       k9s
     ];
+
+    systemd.services.flux-operator-install = {
+      description = "Install flux-operator via Helm (one-shot)";
+      after = [ "k3s.service" ];
+      requires = [ "k3s.service" ];
+      wantedBy = [ "multi-user.target" ];
+
+      script = ''
+
+        # Skip if the release is already deployed
+        if ${pkgs.kubernetes-helm}/bin/helm status flux-operator --namespace flux-system; then
+          echo "flux-operator is already installed, skipping."
+          exit 0
+        fi
+
+        echo "Installing flux-operator via Helm..."
+        ${pkgs.kubernetes-helm}/bin/helm install flux-operator \
+          oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator \
+          --namespace flux-system \
+          --create-namespace
+        echo "flux-operator installation complete."
+      '';
+
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        User = "root";
+      };
+    };
   };
 }
