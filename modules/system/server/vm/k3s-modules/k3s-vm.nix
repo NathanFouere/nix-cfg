58a717bf77b0c5342541b6ab31bdaa1f55420d90
Fouere Nathan <nathanfouere@tutanota.com>
2026-03-25 21:00:32 +0100

    update separation of files

--- modules/system/server/vm/k3s-modules/k3s-vm.nix
+++ modules/system/server/vm/k3s-modules/k3s-vm.nix
@@ -56,23 +56,6 @@
         PasswordAuthentication = false;
       };
     };
-    ## TODO => vérifier si besoin sur toutes les machines ou que le server
-    networking.firewall = {
-      enable = true;
-      # cf . https://github.com/rorosen/k3s-nix <3
-      checkReversePath = false;
-      allowedTCPPorts = [
-        22 # SSH
-        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-        443
-        30000 # Traefik HTTP NodePort (for Let's Encrypt HTTP challenge)
-        30001 # Traefik HTTPS NodePort
-      ];
-      allowedUDPPorts = [
-        8472 # k3s, flannel: required if using multi-node for inter-node networking
-      ];
-      trustedInterfaces = [ "cni+" "flannel.1" ];
-    };
 
     environment.systemPackages = with pkgs; [
       htop
--- modules/system/server/vm/k3s-modules/server/firewall.nix
+++ modules/system/server/vm/k3s-modules/server/firewall.nix
@@ -1,28 +1,20 @@
-{ serverAddr }:
 {
   config = {
+    ## TODO => vérifier si besoin sur toutes les machines ou que le server
     networking.firewall = {
       enable = true;
       # cf . https://github.com/rorosen/k3s-nix <3
       checkReversePath = false;
       allowedTCPPorts = [
         22 # SSH
+        6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+        443
         30000 # Traefik
       ];
       allowedUDPPorts = [
         8472 # k3s, flannel: required if using multi-node for inter-node networking
       ];
       trustedInterfaces = [ "cni+" "flannel.1" ];
-    };
-
-    services.k3s = {
-      enable = true;
-      role = "agent";
-      tokenFile = "/run/agenix/k3s-token";
-      inherit serverAddr;
-      extraFlags = toString [
-        "--debug" # Optionally add additional args to k3s
-      ];
     };
   };
 }
