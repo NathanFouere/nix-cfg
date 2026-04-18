e78df265c78132e44ea71fa932e8ca9a78f404c5
Fouere Nathan <nathanfouere@tutanota.com>
2026-04-18 19:25:59 +0200

    update

--- modules/system/server/vm/k3s-modules/server/cloudflared.nix
+++ modules/system/server/vm/k3s-modules/server/cloudflared.nix
@@ -7,13 +7,13 @@
           credentialsFile = "/run/agenix/cloudflared-tunnel-cred";
           default = "http_status:404";
           ingress = {
-            "traefik.nathan-fouere.com" = "https://localhost:30001";
-            "flux.nathan-fouere.com" = "https://localhost:30001";
-            "api-strategia.nathan-fouere.com" = "https://localhost:30001";
-            "strategia.nathan-fouere.com" = "https://localhost:30001";
-            "president-challenge.nathan-fouere.com" = "https://localhost:30001";
-            "minio-president-challenge.nathan-fouere.com" = "https://localhost:30001";
-            "minio-console-president-challenge.nathan-fouere.com" = "https://localhost:30001";
+            "traefik.nathan-fouere.com" = "http://localhost:30000";
+            "flux.nathan-fouere.com" = "http://localhost:30000";
+            "api-strategia.nathan-fouere.com" = "http://localhost:30000";
+            "strategia.nathan-fouere.com" = "http://localhost:30000";
+            "president-challenge.nathan-fouere.com" = "http://localhost:30000";
+            "minio-president-challenge.nathan-fouere.com" = "http://localhost:30000";
+            "minio-console-president-challenge.nathan-fouere.com" = "http://localhost:30000";
           };
         };
       };
