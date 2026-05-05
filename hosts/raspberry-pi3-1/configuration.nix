42d9d2f9a87942e6615f6648db765d53876d768d
Nathan Fouéré <nathanfouere@tutanota.com>
2026-05-05 22:22:13 +0200

    update

--- hosts/raspberry-pi3-1/configuration.nix
+++ hosts/raspberry-pi3-1/configuration.nix
@@ -1,5 +1,6 @@
 {
   inputs,
+  pkgs,
   ...
 }:
 
@@ -29,28 +30,20 @@
 
   # NixOS wants to enable GRUB by default
   boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = false;
+
+  # Use the generic extlinux bootloader (replacement for removed raspberryPi loader)
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   # if you have a Raspberry Pi 2 or 3, pick this:
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # A bunch of boot parameters needed for optimal runtime on RPi 3b+
   boot.kernelParams = ["cma=256M"];
-  boot.loader.raspberryPi.enable = true;
-  boot.loader.raspberryPi.version = 3;
-  boot.loader.raspberryPi.uboot.enable = true;
-  boot.loader.raspberryPi.firmwareConfig = ''
-    gpu_mem=256
-  '';
   environment.systemPackages = with pkgs; [
-    raspberrypi-tools
+    libraspberrypi
+    raspberrypi-eeprom
   ];
-
-  # File systems configuration for using the installer's partition layout
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-    };
-  };
 
   # Preserve space by sacrificing documentation and history
   documentation.nixos.enable = false;
@@ -61,10 +54,6 @@
   # Configure basic SSH access
   services.openssh.enable = true;
   services.openssh.permitRootLogin = "yes";
-
-  # Use 1GB of additional swap memory in order to not run out of memory
-  # when installing lots of things while running other things at the same time.
-  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 
   # cf . https://mynixos.com/nixpkgs/option/nix.settings.trusted-users
   nix.settings.trusted-users = [
