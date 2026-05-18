2a65193f6fa34a6f29387de73d1c14a3d3732fb1
Fouere Nathan <nathanfouere@tutanota.com>
2026-05-18 19:49:21 +0200

    update cloudflared

--- modules/system/server/vm/k3s-modules/server/cloudflared.nix
+++ modules/system/server/vm/k3s-modules/server/cloudflared.nix
@@ -17,6 +17,11 @@
             "rustfs-console-president-challenge.nathan-fouere.com" = "http://localhost:30000";
             "siyuan.nathan-fouere.com" = "http://localhost:30000";
             "baikal.nathan-fouere.com" = "http://localhost:30000";
+            "jellyfin.nathan-fouere.com" = "http://localhost:30000";
+            "radarr.nathan-fouere.com" = "http://localhost:30000";
+            "sonarr.nathan-fouere.com" = "http://localhost:30000";
+            "prowlarr.nathan-fouere.com" = "http://localhost:30000";
+            "qbittorrent.nathan-fouere.com" = "http://localhost:30000";
             "nathan-fouere.com" = "http://localhost:30000";
           };
         };
