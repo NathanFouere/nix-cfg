92ce2f9fbf5523803b8fe55e5b09ede9e97520c8
Nathan Fouéré <nathanfouere@tutanota.com>
2026-07-04 23:59:30 +0200

    update

--- modules/system/normal/stylix.nix
+++ modules/system/normal/stylix.nix
@@ -3,17 +3,27 @@
   ...
 }:
 {
-  fonts.packages = with pkgs; [ _0xproto ];
+  fonts.packages = with pkgs; [
+    _0xproto
+    bibata-cursors
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+  ];
   stylix.enable = true;
-  stylix.targets.qt.enable = false;
+  stylix.targets.qt.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/oxocarbon-light.yaml";
   stylix.image = ../../../assets/wallpaper/light.png;
   stylix.polarity = "light";
+  stylix.cursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 22;
+  };
   stylix.opacity = {
     desktop = 0.9;
-    applications = 0.9;
-    terminal = 0.9;
-    popups = 0.9;
+    applications = 0.95;
+    terminal = 0.85;
+    popups = 1.0;
   };
   stylix.fonts = {
     serif = {
