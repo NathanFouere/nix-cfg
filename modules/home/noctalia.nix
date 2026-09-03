af8cfa732b8f56f9b75d161263b55c4ace3b8565
Fouere Nathan <nathanfouere@tutanota.com>
2026-09-04 00:26:42 +0200

    update

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -13,6 +13,13 @@
     enable = true;
 
     settings = {
+      shell.corner_radius_scale = 0;
+
+      bar.default = {
+        margin_ends = 0;
+        radius = 0;
+      };
+
       wallpaper = {
         enabled = true;
         default.path = "${config.stylix.image}";
